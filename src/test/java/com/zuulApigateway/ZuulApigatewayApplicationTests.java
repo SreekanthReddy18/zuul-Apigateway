5adec37aa47db5e06e5f5a1dd8840ac206be13af
Sreekanth Reddy <sreekanth3143@gmail.com>
2021-07-27 23:14:36 +0530

    initial commit

---
+++ src/test/java/com/zuulApigateway/ZuulApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuulApigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
